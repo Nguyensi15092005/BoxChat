cb219fbb38ad26f3e6564cdb9d617b7e420aead4
Nguyensi15092005 <142871405+Nguyensi15092005@users.noreply.github.com>
2025-05-20 20:33:03 +0700

    Cấu hình chung

---
+++ index.ts
@@ -0,0 +1,45 @@
+import dotenv from 'dotenv';
+import express, { Express } from "express";
+import * as database from "./config/database";
+
+// flash
+import flash from "express-flash";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+
+dotenv.config();
+
+
+database.connect();
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+// flash
+app.use(cookieParser('SISISISISISISI'));
+app.use(session({ cookie: { maxAge: 60000 } }));
+app.use(flash());
+// end flash
+
+// dùng đc rep.body
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// hết dùng đc rep.body
+
+// Nhúng file tĩnh
+app.use(express.static("./public"));
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+// Routes
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
+
